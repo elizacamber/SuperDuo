0070e190f0cdc888d50ed90851cc6921f9e5ffc9
Eliza Camber <elizacamber@gmail.com>
2015-12-10 23:02:41 +0200

    Fix bugs
    Fix bugs

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -14,12 +14,12 @@ public class Utilies
     {
         switch (league_num)
         {
-            case SERIE_A : return "Seria A";
-            case PREMIER_LEGAUE : return "Premier League";
-            case CHAMPIONS_LEAGUE : return "UEFA Champions League";
-            case PRIMERA_DIVISION : return "Primera Division";
-            case BUNDESLIGA : return "Bundesliga";
-            default: return "Not known League Please report";
+            case SERIE_A : return String.valueOf((R.string.seriaa));
+            case PREMIER_LEGAUE : return String.valueOf(R.string.premierleague);
+            case CHAMPIONS_LEAGUE : return String.valueOf(R.string.champions_league);
+            case PRIMERA_DIVISION : return String.valueOf(R.string.primeradivison);
+            case BUNDESLIGA : return String.valueOf(R.string.bundesliga);
+            default: return String.valueOf(R.string.not_known);
         }
     }
     public static String getMatchDay(int match_day,int league_num)
@@ -28,28 +28,28 @@ public class Utilies
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return R.string.group_stage_text+ ", "+ R.string.matchday_text + " : 6";
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return String.valueOf(R.string.first_knockout_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return String.valueOf(R.string.quarter_final);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return String.valueOf(R.string.semi_final);
             }
             else
             {
-                return "Final";
+                return String.valueOf(R.string.final_text);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return R.string.matchday_text + " : " + String.valueOf(match_day);
         }
     }
 
